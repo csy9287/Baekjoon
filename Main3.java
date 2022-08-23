8cba3be44866d1c5b5034d6f6d10dd39625e2346
csy9287 <77379794+csy9287@users.noreply.github.com>
2022-08-23 23:53:56 +0900

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,18 @@
+package bj0823;
+
+import java.util.Scanner;
+
+public class Main3 {
+	public static void main(String[] args) {
+		
+		// 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램 작성
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println((double)A/B);
+		
+		
+	}
+}
